b8579e6c8c1fa1a0f8b8fdba1b447fc27b565520
Tajveez <Tajveez786@gmail.com>
2021-10-01 23:48:25 +0500

    Setting up Application

--- core/Router.php
+++ core/Router.php
@@ -4,7 +4,14 @@ namespace app\core;
 
 class Router
 {
+    protected $routes = [];
+
     public function __construct()
     {
+    }
+
+    public function run($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
     }
 }
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
 use app\core\Application;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new Application();
 
-// https://youtu.be/6ERdu4k62wI?t=1167
+$app->router->get('/', function () {
+    return "Hello World";
+});
