a8d37032771b869faa98f1be2607f1d93b49fa88
Tajveez <Tajveez786@gmail.com>
2021-10-16 01:07:26 +0500

    Making post route

--- public/index.php
+++ public/index.php
@@ -12,5 +12,8 @@ $app = new Application(dirname(__DIR__));
 $app->router->get('/', 'home');
 
 $app->router->get('/contact', 'contact');
+$app->router->post('/contact', function () {
+    echo "Handling Content...";
+});
 
 $app->run();
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -31,7 +31,9 @@
             </div>
         </div>
     </nav>
-    {{content}}
+    <div class="container">
+        {{content}}
+    </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
