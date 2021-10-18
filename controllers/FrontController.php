b0ed0661f8a0bdb34a7bc25203f51c7b26e347d9
Tajveez <Tajveez786@gmail.com>
2021-10-18 23:10:38 +0500

    Rendering view using controller

--- controllers/FrontController.php
+++ controllers/FrontController.php
@@ -2,11 +2,18 @@
 
 namespace app\controllers;
 
+use app\core\Application;
+
 class FrontController
 {
     public function action()
     {
         return "Handling action";
+    }
+
+    public function renderContact()
+    {
+        return Application::$app->router->renderView('contact');
     }
 
     public function handlePost()
--- public/index.php
+++ public/index.php
@@ -11,8 +11,7 @@ use app\core\Application;
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
-
-$app->router->get('/contact', 'contact');
+$app->router->get('/contact', [FrontController::class, 'renderContact']);
 $app->router->post('/contact', [FrontController::class, 'handlePost']);
 
 $app->run();
