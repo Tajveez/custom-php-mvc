b1fe4b4d4350198a3dbc46565479ae56a5c6e873
Tajveez <Tajveez786@gmail.com>
2021-10-25 23:49:14 +0500

    creating Model

---
+++ models/RegistrationModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\models;
+
+class RegistrationModel
+{
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+}
