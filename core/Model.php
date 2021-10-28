14b71d3c4586aff7f340bee1d34e0eaa2ff382de
Tajveez <Tajveez786@gmail.com>
2021-10-28 23:59:14 +0500

    Model rules

--- core/Model.php
+++ core/Model.php
@@ -4,6 +4,12 @@ namespace app\core;
 
 abstract class Model
 {
+    public const RULE_REQUIRED = 'required';
+    public const RULE_EMAIL = 'email';
+    public const RULE_MIN = 'min';
+    public const RULE_MAX = 'max';
+    public const RULE_MATCH = 'match';
+
     public function loadData($data)
     {
         foreach ($data as $key => $value) {
