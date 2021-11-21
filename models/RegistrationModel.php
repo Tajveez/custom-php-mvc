a01fc4743ffac19304d3ad7328ade3e04df72a94
Tajveez <Tajveez786@gmail.com>
2021-11-21 23:55:36 +0500

    Validation Rules

--- models/RegistrationModel.php
+++ models/RegistrationModel.php
@@ -10,15 +10,21 @@ class RegistrationModel extends Model
     public string $lastname;
     public string $email;
     public string $password;
+    public string $passwordConfirm;
 
     public function register()
     {
-        # code...
+        echo "Creating a new User.";
     }
 
     public function rules(): array
     {
-        # code...
-        return [];
+        return [
+            'firstname' => [self::RULE_REQUIRED],
+            'lastname' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED],
+            'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+        ];
     }
 }
